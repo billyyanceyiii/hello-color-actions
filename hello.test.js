284d918260acd5a905b7bf0cecfac34fb9c97741
billyyanceyiii <billy.yanceyiii@gmail.com>
2024-09-17 20:38:53 -0500

    commit

--- hello.test.js
+++ hello.test.js
@@ -1,6 +1,6 @@
 const hello = require("./hello");
 describe("My hello", () => {
   test("works", () => {
-    expect(hello.hello()).toEqual("Hello World from the Power Hub");
+    expect(hello.hello()).toEqual("Hello World from the Power Hub!");
   });
 });
